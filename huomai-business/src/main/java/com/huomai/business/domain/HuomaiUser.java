559058560565a08353640aa3e5e5f2684d4fba13
chenshf <15921471241@163.com>
2021-06-21 15:51:43 +0800

    登录授权

--- huomai-business/src/main/java/com/huomai/business/domain/HuomaiUser.java
+++ huomai-business/src/main/java/com/huomai/business/domain/HuomaiUser.java
@@ -1,9 +1,6 @@
 package com.huomai.business.domain;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
@@ -29,7 +26,7 @@ public class HuomaiUser implements Serializable {
 	/**
 	 * 用户Id
 	 */
-	@TableId(value = "user_id")
+	@TableId(value = "user_id",type = IdType.AUTO)
 	private Long userId;
 
 	/**

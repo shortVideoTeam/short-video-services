762aa476f2cac9899cf128f632b815eb52e8d748
chenshf <15921471241@163.com>
2021-06-27 17:33:03 +0800

    下单

--- huomai-app/src/main/java/com/huomai/app/controller/order/HuomaiRechargeController.java
+++ huomai-app/src/main/java/com/huomai/app/controller/order/HuomaiRechargeController.java
@@ -21,10 +21,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -49,7 +46,7 @@ public class HuomaiRechargeController {
 	}
 
 	@ApiOperation("充值接口")
-	@GetMapping("/recharge")
+	@PostMapping("/recharge")
 	public AjaxResult recharge(@Validated @RequestBody HuomaiRechargeAddBo bo) {
 		return orderService.recharge(bo);
 	}
